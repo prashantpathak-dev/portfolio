3903dbb59bbe3a36e560e5cbda3aa987b4a4ede6
Prashant Pathak <prashantpathak.dev@gmail.com>
2025-02-25 07:39:03 +0530

    feat(performance): :sparkles: add support for vercel speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 inject();
+injectSpeedInsights();
 
 export const load = () => {
 	return {};
