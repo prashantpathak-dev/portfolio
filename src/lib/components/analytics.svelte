ffd35cd47dbce8a55d2e97163bd88c21b4a1fe01
Prashant Pathak <prashantpathak.dev@gmail.com>
2025-02-25 08:19:06 +0530

    feat(analytics): :sparkles: add support for google analytics

---
+++ src/lib/components/analytics.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y0JN5F5SV6"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-Y0JN5F5SV6');
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,12 @@
 	let { children } = $props();
 
 	import Head from '$lib/components/head.svelte';
+	import Analytics from '$lib/components/analytics.svelte';
 </script>
 
 <Head />
+
+<Analytics />
 
 <main class="bg-base flex h-screen w-screen flex-col">
 	{@render children()}
